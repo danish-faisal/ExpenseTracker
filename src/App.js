84ec26e9924354ce751928abd8c8b20e38cbbf91
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-02-06 16:23:02 +0530

    IncomeExpenses component & Styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Balance } from './components/Balance';
 import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
       <Header />
       <div className="container">
-      <Balance />
+        <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
